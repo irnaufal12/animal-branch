76c14529e8c8fa59b76eeff3147dd78ff75017d0
Naufal Ihsanul Rasyid <61204959+irnaufal12@users.noreply.github.com>
2022-12-20 11:49:47 +0700

    fourth-commit

---
+++ QuizHari2/Nomor1/ManajerSalesClass.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizHari2.Nomor1
+{
+    internal class ManajerSalesClass : PresidenClass
+    {
+        public ManajerSalesClass() : base()
+        {
+            this.position = "Manajer Sales";
+        }
+    }
+}
--- QuizHari2/Program.cs
+++ QuizHari2/Program.cs
@@ -21,7 +21,7 @@ namespace QuizHari2
             PresidenClass presidenClass = new PresidenClass();
             presidenClass.Name = "Nama Presiden";
             presidenClass.Address = "Alamat Presiden";
-            presidenClass.BirthDate = "dd-MM-yy";
+            presidenClass.BirthDate = "dd/MM/yyyy";
 
             Console.WriteLine(presidenClass.Name);
             Console.WriteLine(presidenClass.getPosition());
@@ -29,9 +29,9 @@ namespace QuizHari2
             Console.WriteLine(presidenClass.BirthDate);
 
             ManajerITClass manajerITClass = new ManajerITClass();
-            manajerITClass.Name= "Nama Manajer";
+            manajerITClass.Name= "Nama Manajer IT";
             manajerITClass.Address = "Alamat";
-            manajerITClass.BirthDate = "dd-MM-yy";
+            manajerITClass.BirthDate = "dd/MM/yyyy";
 
             Console.WriteLine();
             Console.WriteLine(manajerITClass.Name);
@@ -39,16 +39,52 @@ namespace QuizHari2
             Console.WriteLine(manajerITClass.Address);
             Console.WriteLine(manajerITClass.BirthDate);
 
-            ProgrammerClass programmerClass = new ProgrammerClass();
-            programmerClass.Name = "Nama Programmer";
-            programmerClass.Address = "Alamat";
-            programmerClass.BirthDate = "dd-MM-yy";
+            List<ProgrammerClass> programmerClassLists = new List<ProgrammerClass>();
+            programmerClassLists.Add(new ProgrammerClass()
+            {
+                Name = "Nama Programmer",
+                Address = "Alamat",
+                BirthDate = "dd/MM/yyyy"
+            });
+
+            foreach (ProgrammerClass programmerClassList in programmerClassLists)
+            {
+                Console.WriteLine();
+                Console.WriteLine(programmerClassList.Name);
+                Console.WriteLine(programmerClassList.getPosition());
+                Console.WriteLine(programmerClassList.Address);
+                Console.WriteLine(programmerClassList.BirthDate);
+            }
+
+
+            ManajerSalesClass manajerSalesClass = new ManajerSalesClass();
+            manajerSalesClass.Name = "Nama Manajer Sales";
+            manajerSalesClass.Address = "Alamat";
+            manajerSalesClass.BirthDate = "dd/MM/yyyy";
 
             Console.WriteLine();
-            Console.WriteLine(programmerClass.Name);
-            Console.WriteLine(programmerClass.getPosition());
-            Console.WriteLine(programmerClass.Address);
-            Console.WriteLine(programmerClass.BirthDate);
+            Console.WriteLine(manajerSalesClass.Name);
+            Console.WriteLine(manajerSalesClass.getPosition());
+            Console.WriteLine(manajerSalesClass.Address);
+            Console.WriteLine(manajerSalesClass.BirthDate);
+
+            List<SalesClass> salesClassLists = new List<SalesClass>();
+            salesClassLists.Add(new SalesClass()
+            {
+                Name = "Nama Sales",
+                Address = "Alamat Sales",
+                BirthDate = "dd/MM/yyyy"
+            });
+
+            foreach (SalesClass salesClassList in salesClassLists)
+            {
+                Console.WriteLine();
+                Console.WriteLine(salesClassList.Name);
+                Console.WriteLine(salesClassList.getPosition());
+                Console.WriteLine(salesClassList.Address);
+                Console.WriteLine(salesClassList.BirthDate);
+            }
+
 
         }
     }
